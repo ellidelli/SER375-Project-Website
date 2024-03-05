63e47d9c0ee5c945bdbe5197d4e351ba4a984820
ellidelli <elizabeth.delea@gmail.com>
2024-03-05 15:43:11 -0500

    initial commit

---
+++ src/components/twoColumnTemplate.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="data">
+        <h1>{{ projectTitle }}</h1>
+        <h3>{{ author }}</h3>
+        <div class="container">
+            <div class="left">
+                <p>{{ description }}</p>
+                <p>{{ story }}</p>
+                <p>{{ struggles }}</p>
+                <p>{{ triumphs }}</p>
+            </div>
+            <div class="right">
+                <div v-for="(picture, index) in pictures" :key="index">
+                    <img :src="picture" alt="Project Image">
+                </div>
+                <video :src="video" controls></video>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import projectData from '../projects/projects.json'
+export default {
+    mounted() {
+        console.log(this.$route.params.name)
+    },
+    props: {
+        backgroundColor: String,
+        projectTitle: String,
+        author: String,
+        description: String,
+        story: String,
+        struggles: String,
+        triumphs: String,
+        pictures: Array,
+        video: String
+    }
+}
+</script>
+
+<style scoped>
+.data {
+    margin-top: 10vh;
+}
+
+.container {
+    display: flex;
+    flex-direction: row;
+}
+
+.left {
+    width: 50%;
+    float: left;
+    display: flex;
+    flex-direction: column;
+}
+
+.right {
+    width: 50%;
+    float: right;
+    display: flex;
+    flex-direction: column;
+}
+
+.right img {
+    width: fit-content;
+    max-width: 90%;
+}
+
+.right video {
+    width: 90%;
+}
+
+h1,
+h2,
+h3 {
+    color: white;
+}
+
+h3 {
+    padding-bottom: 5vh;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DiscoverView from '../views/DiscoverView.vue'
+import twoColumnTemplate from '@/components/twoColumnTemplate.vue'
 
 const routes = [
   {
@@ -8,13 +10,37 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/discover',
+    name: 'discover',
+    component: DiscoverView
+  }, 
+  {
+    path: '/template',
+    name: 'template',
+    component: twoColumnTemplate
+  },
+  {
+    path: '/project/:name',
+    name: 'project-name',
+    component: twoColumnTemplate,
+    //props: ['projectName']
+  },
+
+  // {
+  //   path: '/project/jacobhogrefe-minnecraft-mod',
+  //   name: 'jacob-HOGrefe',
+  //   component: some-component,
+  // },
+  // {
+  //   path: '/project/elliedelli-stardew-mod',
+  //   name: 'EL-DEL',
+  //   component: some-component,
+  // },
+  // {
+  //   path: '/project/elliedelli-this-webside',
+  //   name: 'EL-DEL',
+  //   component: some-component,
+  // }
 ]
 
 const router = createRouter({
